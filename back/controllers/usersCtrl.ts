3c7f1775f273fefd80e589d40d628511929976cb
Gaspachoo <gaspard.lefort@outlook.fr>
2025-11-25 20:40:20 +0100

    [BACK] add try/catch to usersCtrl

--- back/controllers/usersCtrl.ts
+++ back/controllers/usersCtrl.ts
@@ -3,18 +3,34 @@ import usersService from '../services/usersService';
 
 const usersCtrl = {
   createUser: async (req: Request, res: Response) => {
-    const user = await usersService.createUser(req);
-    return res.status(201).json(user);
+    try {
+        const user = await usersService.createUser(req);
+        return res.status(201).json(user);
+    } catch(err: any) {
+        const message = err?.meta?.driverAdapterError?.cause?.originalMessage || err.message || 'Unknown error';
+        return res.status(500).json({ error: 'Fail to create user', details: message });
+    }    
     },
 
     getUsers: async (req: Request, res: Response) => {
-      const users = await usersService.getUsers();
-      return res.status(200).json(users);
+        try{
+
+                const users = await usersService.getUsers();
+                return res.status(200).json(users);
+        } catch(err: any) {
+                const message = err?.meta?.driverAdapterError?.cause?.originalMessage || err.message || 'Unknown error';
+                return res.status(500).json({ error: 'Fail to get users', details: message });
+        }
     },
 
     deleteUser: async (req: Request, res: Response) => {
-        const user = await usersService.deleteUser(Number(req.params.id));
-        return res.status(200).json(user);
+        try{
+            const user = await usersService.deleteUser(Number(req.params.id));
+            return res.status(200).json(user);
+        } catch(err: any) {
+            const message = err?.meta?.driverAdapterError?.cause?.originalMessage || err.message || 'Unknown error';
+            return res.status(500).json({ error: 'Fail to delete user', details: message });
+        }
     }
 };
 
