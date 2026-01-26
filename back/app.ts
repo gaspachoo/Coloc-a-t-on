94a20f0deb1c605a43c5240c25d250e1bccc6db0
Gaspachoo <gaspard.lefort@outlook.fr>
2026-01-26 21:39:18 +0100

    [BACK] add cors policies for frontend to call on default port

--- back/app.ts
+++ back/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import routes from './routes';
 import path from 'path';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { fileURLToPath } from 'url';
 
@@ -10,11 +11,27 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 5000;
 
+const FRONT_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+const corsOptions = {
+  origin: FRONT_URL,
+  credentials: true,
+  methods: ['GET', 'PATCH', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authorization',
+  ],
+  optionsSuccessStatus: 200,  // réponse aux pré-vol
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 app.use('/api', routes);
 
 app.get('/', (req: Request, res: Response) => {
